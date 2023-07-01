fb548043de334765970b3b5f126f757788aebdc9
Your Name <Jmlsoteyza.dev@gmail.com>
2023-07-01 12:47:56 +0400

    Navbar responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../../assets/images/jom-logo.svg";
 import Logo1 from "../../assets/images/jom-logo1.svg";
@@ -8,21 +8,33 @@ import { ThemeFunction } from "../../App";
 
 const Navbar = () => {
   const { theme, toggleTheme } = useContext(ThemeFunction);
+
+  const [show, setShow] = useState(false);
+
+  const handleCloseNav = () => {
+    setShow(false);
+  };
+
+  const handleNavClick = () => {
+    setShow(!show);
+  };
+
   return (
     <nav>
-      <Link to="/">
+      <Link className="logo123" to="/" onClick={handleCloseNav}>
         {theme === "dark" ? (
           <img src={Logo} alt={Logo} className="logo" />
         ) : (
           <img src={Logo1} alt={Logo1} className="logo" />
         )}
       </Link>
-      <ul>
+      <ul className={show ? "active" : ""}>
         <NavLink
           className={({ isActive }) =>
             isActive ? "Link-color active" : "Link-color"
           }
           to="About"
+          onClick={handleCloseNav}
         >
           <li className="line line-color">About</li>
         </NavLink>
@@ -32,6 +44,7 @@ const Navbar = () => {
             isActive ? "Link-color active" : "Link-color"
           }
           to="Projects"
+          onClick={handleCloseNav}
         >
           <li className="line line-color">Projects</li>
         </NavLink>
@@ -40,12 +53,13 @@ const Navbar = () => {
             isActive ? "Link-color active" : "Link-color"
           }
           to="Contacts"
+          onClick={handleCloseNav}
         >
           <li className="line line-color">Contacts</li>
         </NavLink>
         <div className="toggle-mode">
           {theme === "dark" ? (
-            <img src={light} alt="moon" onClick={toggleTheme} className="sun"/>
+            <img src={light} alt="moon" onClick={toggleTheme} className="sun" />
           ) : (
             <img
               src={dark}
@@ -56,6 +70,26 @@ const Navbar = () => {
           )}
         </div>
       </ul>
+      <div className="toggle-mode mode-off">
+        {theme === "dark" ? (
+          <img src={light} alt="moon" onClick={toggleTheme} className="sun" />
+        ) : (
+          <img
+            src={dark}
+            alt="sun"
+            onClick={toggleTheme}
+            className="second-img"
+          />
+        )}
+      </div>
+      <div
+        className={show ? "toggle active" : "toggle"}
+        onClick={handleNavClick}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </nav>
   );
 };
